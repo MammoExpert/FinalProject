634d0616963312990f799905f9f24a0e607f4533
vadimbulavski <zolsberh@mail.ru>
2017-07-24 18:48:16 +0300

    Add UC Connection to Worklist

---
+++ MammoExpert/MammoExpert.PatientServices.UI.Controls/ViewModel/WorklistConnectionConfigurationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MammoExpert.PatientServices.UI.Controls.ViewModel
+{
+    public class WorklistConnectionConfigurationModel
+    {
+        public WorklistConnectionConfigurationModel() { }
+    }
+}
