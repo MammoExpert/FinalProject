98aaef34029d06d677bbecd49ca5e8fd36fd35cf
pirozhnikolga <pirozhenka@yandex.ru>
2017-09-07 21:20:55 +0300

    Tests project

---
+++ MammoExpert/MammoExpert.PatientServices.Tests/Demo/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace MammoExpert.PatientServices.Tests.Demo
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
